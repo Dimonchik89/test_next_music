62ea0e265e36472a2c4ae0bdd8015be74bbf194e
Dimonchik89 <dima.music1989@gmail.com>
2023-02-15 12:28:46 +0200

    change music link

--- api/playApi.js
+++ api/playApi.js
@@ -1,6 +1,6 @@
 
 export const generateMusicLink = async (serverLink) => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}music/download?filename=${serverLink}`)
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/music/download?filename=${serverLink}`)
     const blob = await response.blob()
     const musicUrl = window.URL.createObjectURL(blob)
     return musicUrl;
