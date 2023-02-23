1e229a65c2c4662ec97e0d0ce29f89151ec4f275
Dimonchik89 <dima.music1989@gmail.com>
2023-02-23 14:10:18 +0200

    debug send link to email in modal

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -1,7 +1,7 @@
 
 export const sendEmail = async (link) => {
     const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}email`, 
+        `${process.env.NEXT_PUBLIC_BASE_URL}/email`, 
         {
             method: 'POST',
             headers: {"Content-type": "application/json"},
