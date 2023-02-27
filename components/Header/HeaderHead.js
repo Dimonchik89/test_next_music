678cc07aa85cd878d0071d5de4d00401f1bd66c5
Dimonchik89 <dima.music1989@gmail.com>
2023-02-27 09:27:39 +0200

    hide header pro button

--- components/Header/HeaderHead.js
+++ components/Header/HeaderHead.js
@@ -44,11 +44,12 @@ const HeaderHead = ({showPlayer, mobileMenu, mobileSearch, toggleMobileMenu, tog
                     />
                 </picture>
                 {search}
-                {/* <button
+                <button
                     className={`${button.border_gray_text_yellow} ${header.header__button}`}
+                    // opacity: 0
                 >
                     Pro Catalog
-                </button> */}
+                </button>
                 <HeaderMobile 
                     toggleMobileMenu={toggleMobileMenu} 
                     toggleMobileSearch={toggleMobileSearch}
