b3773ee3814d5ac1d7bcce9175011e1a4ac39c97
Dimonchik89 <dima.music1989@gmail.com>
2023-03-02 15:00:03 +0200

    change footer text and change footer and main padding

--- components/Footer/FooterHead.js
+++ components/Footer/FooterHead.js
@@ -9,15 +9,15 @@ const FooterHead = () => {
     return (
         <Box>
             <h2 className={`${footer.title}`}>
-                 Check Out the Latest Royalty Free Music Collections
+                 Follow us:
             </h2>
-            <Box className={footer.text__wrapper}>
+            {/* <Box className={footer.text__wrapper}>
                 <p className={`${footer.text}`}>
                     I`&apos;`ve compiled themed collections of production music libraries for your media projects, so you don`&apos;`t have to search 
                     endlessly for the perfect track. With TakeTones, all the royalty-free music you need is in one place.
                         Here are four of the latest collections compiled by our experts. Take a listen!
                 </p>
-            </Box>
+            </Box> */}
         </Box>
     )
 }
