6a4025adfbb55497897d88b5ade72bd3fcbcf9e0
Dimonchik89 <dima.music1989@gmail.com>
2023-03-17 07:45:30 +0200

    hide devtoolsin production mode

--- store/store.js
+++ store/store.js
@@ -17,7 +17,8 @@ const store = configureStore({
         category,
         search
     },
-    middleware: getDefaultMiddleware => getDefaultMiddleware()
+    middleware: getDefaultMiddleware => getDefaultMiddleware(),
+    devTools: process.env.NODE_ENV !== 'production',
 })
 
 export default store;
