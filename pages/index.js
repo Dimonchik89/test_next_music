df720252a01bbbcf7baa57569a85e4cbb96b33b1
Dimonchik89 <dima.music1989@gmail.com>
2023-04-21 09:07:38 +0300

    add to index header meta-teg security-policy

--- pages/index.js
+++ pages/index.js
@@ -57,6 +57,7 @@ const Home = ({category, checkRole, serverAudio, addAllCategory, selectActualCat
           <link rel="icon" type="image/png" sizes="16x16" href="../static/favicon/favicon-16x16.png"/>
           <link rel="manifest" href="../static/favicon/site.webmanifest"/>
           <link rel="mask-icon" href="../static/favicon/safari-pinned-tab.svg" color="#5bbad5"/>
+          <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
           <meta name="msapplication-TileColor" content="#da532c"/>
           <meta name="theme-color" content="#ffffff"/>
           <meta name="robots" content="noindex"/>
